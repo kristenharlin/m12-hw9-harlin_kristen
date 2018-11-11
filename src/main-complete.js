2dc2af7d5d97d4d37e6eaa0d556ea81f8286a39a
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-11-11 07:52:51 -0500

    Write thisButtonText and thisButtonLength variables, Log 'hi' to console

--- src/main-complete.js
+++ src/main-complete.js
@@ -2,10 +2,11 @@ const navButtons = document.querySelectorAll('#navbar ul li');
 
 function wrapper() {
   const loopThrough = () => {
-
+    let thisButtonText = this.childNodes[0].nodeValue;
+    let thisButtonLength = thisButtonText.length;
 
     for (let i = 0; i < thisButtonLength; i++) {
-
+      console.log('hi');
     }
 
   };
@@ -13,5 +14,5 @@ function wrapper() {
 };
 
 for(let i = 0; i < navButtons.length; i++) {
-
+  navButtons[i].addEventListener('click', wrapper);
 };
